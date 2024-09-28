c5fef7764df629ee355dc961ddeecba0d80d75f4
HellZDarkArkon <maximumwerewolf@gmail.com>
2024-09-28 00:14:52 -0400

    Began Implementation of XENO_METAL_SWORD, XENO_METAL_PICKAXE, XENO_METAL_SHOVEL, XENO_METAL_AXE and XENO_METAL_HOE in PhageItems.java, added PhageToolTiers.java to begin ToolTier implementation.

--- src/main/java/org/net/nanophage/item/PhageItems.java
+++ src/main/java/org/net/nanophage/item/PhageItems.java
@@ -1,6 +1,6 @@
 package org.net.nanophage.item;
 
-import net.minecraft.world.item.Item;
+import net.minecraft.world.item.*;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.*;
 import org.net.nanophage.Nanophage;
@@ -13,6 +13,13 @@ public class PhageItems {
             ()->new Item(new Item.Properties()));
     public static final RegistryObject<Item> XENO_METAL_NUGGET = ITEM_REGISTER.register("xeno_metal_nugget",
             ()->new Item(new Item.Properties()));
+
+    //Tools
+    public static final RegistryObject<Item> XENO_METAL_SWORD = ITEM_REGISTER.register("xeno_metal_sword", ()->new SwordItem(PhageToolTiers.XENO_METAL, 2,3, new Item.Properties().durability(256)));
+    public static final RegistryObject<Item> XENO_METAL_PICKAXE = ITEM_REGISTER.register("xeno_metal_pickaxe", ()->new PickaxeItem(PhageToolTiers.XENO_METAL, 2, 3, new Item.Properties().durability(256)));
+    public static final RegistryObject<Item> XENO_METAL_SHOVEL = ITEM_REGISTER.register("xeno_metal_shovel", ()->new ShovelItem(PhageToolTiers.XENO_METAL, 2, 3, new Item.Properties().durability(256)));
+    public static final RegistryObject<Item> XENO_METAL_AXE = ITEM_REGISTER.register("xeno_metal_axe", ()->new AxeItem(PhageToolTiers.XENO_METAL, 2, 3, new Item.Properties().durability(256)));
+    public static final RegistryObject<Item> XENO_METAL_HOE = ITEM_REGISTER.register("xeno_metal_hoe", ()->new HoeItem(PhageToolTiers.XENO_METAL, 2, 3, new Item.Properties().durability(256)));
 
 
 
---
+++ src/main/java/org/net/nanophage/item/PhageToolTiers.java
@@ -0,0 +1,11 @@
+package org.net.nanophage.item;
+
+import net.minecraft.world.item.Tier;
+import net.minecraftforge.common.ForgeTier;
+import net.minecraftforge.common.TierSortingRegistry;
+
+public class PhageToolTiers {
+    public static final Tier XENO_METAL = TierSortingRegistry
+            .registerTier(new ForgeTier(5, 1400, 11f,
+                    3f, 26, ))
+}
